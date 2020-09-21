44d24c8ec50b2241bdc887e1f986cc45d71edf9c
Eugie Limpin <eugiellimpin@gmail.com>
2020-09-22 07:51:19 +0800

    Fetch and display nodes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,32 @@
-import ky from 'ky';
-import React from 'react';
+import ky from "ky";
+import React, { useEffect, useState } from "react";
 
-import './App.css';
+import "./App.css";
+
+interface Node {
+  id: string;
+  title: string;
+}
 
 function App() {
-  ky.get('/nodes');
+  const [nodes, setNodes] = useState<Node[]>([]);
+
+  useEffect(() => {
+    const fetchNodes = async () => {
+      const fetchedNodes: Node[] = await ky.get("/nodes").json();
+      setNodes(fetchedNodes);
+    };
+
+    fetchNodes();
+  }, []);
 
   return (
     <div className="">
-      hello Ada
+      <ul>
+        {nodes.map(({ id, title }) => (
+          <li key={id}>{title}</li>
+        ))}
+      </ul>
     </div>
   );
 }
