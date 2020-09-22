51492c4dbd4aa19ed3a2392537877e574d02a6d1
Eugie Limpin <eugiellimpin@gmail.com>
2020-09-22 20:38:41 +0800

    Fetch node on click and store nodes in a map

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,45 @@ import React, { useEffect, useState } from "react";
 
 import "./App.css";
 
+interface NodeContent {
+  type: "text";
+  body: string;
+}
+
 interface Node {
   id: string;
   title: string;
+  connections?: Node[];
+  content?: NodeContent[];
+}
+
+interface NodesById {
+  [id: string]: Node;
 }
 
 function App() {
-  const [nodes, setNodes] = useState<Node[]>([]);
+  const [nodesById, setNodesById] = useState<NodesById>({});
+
+  const fetchNode = async (id: string) => {
+    // This endpoint returns an array that contains only one node
+    const [fetchedNode] = await ky.get(`/nodes/${id}`).json();
+    console.assert(fetchNode.length === 1);
+
+    setNodesById((prev) => ({
+      ...prev,
+      [fetchedNode.id]: { ...prev[fetchedNode.id], ...fetchedNode },
+    }));
+  };
 
   useEffect(() => {
     const fetchNodes = async () => {
       const fetchedNodes: Node[] = await ky.get("/nodes").json();
-      setNodes(fetchedNodes);
+      const nodesById = fetchedNodes.reduce((db, node) => {
+        db[node.id] = node;
+        return db;
+      }, {} as NodesById);
+
+      setNodesById(nodesById);
     };
 
     fetchNodes();
@@ -23,8 +50,10 @@ function App() {
   return (
     <div className="">
       <ul>
-        {nodes.map(({ id, title }) => (
-          <li key={id}>{title}</li>
+        {Object.values(nodesById).map(({ id, title }) => (
+          <li onClick={() => fetchNode(id)} key={id}>
+            {title}
+          </li>
         ))}
       </ul>
     </div>
